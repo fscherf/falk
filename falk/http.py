537b1ce3e8d2865559b645595facfcd9f4e05d34
Florian Scherf <mail@florianscherf.de>
2025-09-06 23:54:32 +0200

    http: add helper for headers
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- falk/http.py
+++ falk/http.py
@@ -1,6 +1,7 @@
 from falk.errors import InvalidStatusCodeError
 
 
+# status
 def set_status(response, status):
     if 100 < status > 599:
         raise InvalidStatusCodeError(
@@ -8,3 +9,29 @@ def set_status(response, status):
         )
 
     response["status"] = status
+
+
+# headers
+def normalize_header_name(name):
+    return name.title()
+
+
+def set_header(headers, name, value):
+    normalized_header_name = normalize_header_name(name)
+
+    headers[normalized_header_name] = value
+
+
+def get_header(headers, name, default=None):
+    normalized_header_name = normalize_header_name(name)
+
+    if default is not None:
+        return headers.get(normalized_header_name, default)
+
+    return headers.get(normalized_header_name)
+
+
+def del_header(headers, name, value):
+    normalized_header_name = normalize_header_name(name)
+
+    del headers[normalized_header_name]
