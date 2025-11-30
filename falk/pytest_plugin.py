3d82ba2f70bf8bc8b067c32cd8aeb1d89ec58abb
Florian Scherf <mail@florianscherf.de>
2025-11-30 18:52:34 +0100

    pytest: remove aiohttp specific code
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- falk/pytest_plugin.py
+++ falk/pytest_plugin.py
@@ -1,10 +1,7 @@
 from threading import Thread
 import contextlib
-import concurrent
-import asyncio
 import time
 
-from aiohttp.web import AppRunner, TCPSite
 from uvicorn.middleware.wsgi import WSGIMiddleware
 import uvicorn
 import pytest
@@ -12,73 +9,6 @@ import pytest
 from falk.apps import run_configure_app
 from falk.asgi import get_asgi_app
 from falk.wsgi import get_wsgi_app
-
-
-class AiohttpAppRunner:
-    def __init__(self):
-        self._app = None
-        self._site = None
-        self._loop = None
-        self._started = None
-        self._stopped = None
-        self._thread = None
-        self._host = None
-        self._port = None
-
-    async def _run_app(self):
-        runner = AppRunner(self._app)
-
-        await runner.setup()
-
-        self._site = TCPSite(
-            runner=runner,
-            host=self._host,
-            port=self._port,
-        )
-
-        await self._site.start()
-
-        self._started.set_result(None)
-
-        await self._stopped
-
-        await runner.cleanup()
-
-    async def _set_stopped(self):
-        self._stopped.set_result(None)
-
-    def _run_loop_in_thread(self):
-        self._loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self._loop)
-        self._stopped = asyncio.Future(loop=self._loop)
-
-        self._loop.run_until_complete(self._run_app())
-
-    def start(self, app, host="127.0.0.1", port=0):
-        self._app = app
-        self._host = host
-        self._port = port
-        self._started = concurrent.futures.Future()
-
-        self._thread = Thread(
-            target=self._run_loop_in_thread,
-        )
-
-        self._thread.start()
-        self._started.result()
-
-    def stop(self):
-        future = asyncio.run_coroutine_threadsafe(
-            coro=self._set_stopped(),
-            loop=self._loop,
-        )
-
-        future.result()
-
-    def get_base_url(self):
-        host, port = self._site._server.sockets[0].getsockname()
-
-        return f"http://{host}:{port}"
 
 
 @pytest.fixture
