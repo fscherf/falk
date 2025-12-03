4852d418ac38c66c6cc29ca806189e374d6643cb
Florian Scherf <mail@florianscherf.de>
2025-12-03 23:59:57 +0100

    dependencies: middlewares: use correct settings keys in mutable app
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- falk/providers/middlewares.py
+++ falk/providers/middlewares.py
@@ -1,12 +1,12 @@
-def add_pre_component_middleware_provider(app):
+def add_pre_component_middleware_provider(mutable_app):
     def add_pre_component_middleware(middleware):
-        app["pre_component_middlewares"].append(middleware)
+        mutable_app["settings"]["pre_component_middlewares"].append(middleware)
 
     return add_pre_component_middleware
 
 
-def add_post_component_middleware_provider(app):
+def add_post_component_middleware_provider(mutable_app):
     def add_post_component_middleware(middleware):
-        app["post_component_middlewares"].append(middleware)
+        mutable_app["settings"]["post_component_middlewares"].append(middleware)
 
     return add_post_component_middleware
