63e6bf4079d9789416a03e15df9491d325102436
Florian Scherf <mail@florianscherf.de>
2026-04-04 20:05:06 +0200

    dependencies: middlewares: use mutable_settings instead of mutable_app
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- falk/providers/middlewares.py
+++ falk/providers/middlewares.py
@@ -3,37 +3,37 @@ def _check_middleware(middleware):
         raise ValueError("middlewares need to be callable")
 
 
-def add_pre_request_middleware_provider(mutable_app):
+def add_pre_request_middleware_provider(mutable_settings):
     def add_pre_request_middleware(middleware):
         _check_middleware(middleware)
 
-        mutable_app["settings"]["pre_request_middlewares"].append(middleware)
+        mutable_settings["pre_request_middlewares"].append(middleware)
 
     return add_pre_request_middleware
 
 
-def add_post_request_middleware_provider(mutable_app):
+def add_post_request_middleware_provider(mutable_settings):
     def add_post_request_middleware(middleware):
         _check_middleware(middleware)
 
-        mutable_app["settings"]["post_request_middlewares"].append(middleware)
+        mutable_settings["post_request_middlewares"].append(middleware)
 
     return add_post_request_middleware
 
 
-def add_pre_component_middleware_provider(mutable_app):
+def add_pre_component_middleware_provider(mutable_settings):
     def add_pre_component_middleware(middleware):
         _check_middleware(middleware)
 
-        mutable_app["settings"]["pre_component_middlewares"].append(middleware)
+        mutable_settings["pre_component_middlewares"].append(middleware)
 
     return add_pre_component_middleware
 
 
-def add_post_component_middleware_provider(mutable_app):
+def add_post_component_middleware_provider(mutable_settings):
     def add_post_component_middleware(middleware):
         _check_middleware(middleware)
 
-        mutable_app["settings"]["post_component_middlewares"].append(middleware)
+        mutable_settings["post_component_middlewares"].append(middleware)
 
     return add_post_component_middleware
