3fc87dd1ffeafcfc73702a8988a5a2f6a863ec7f
Florian Scherf <mail@florianscherf.de>
2026-04-04 13:12:27 +0200

    test apps: test app: index: remove obsolete template context code
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- test_apps/test_app/components/index.py
+++ test_apps/test_app/components/index.py
@@ -1,11 +1,7 @@
 from test_app.components.base import Base
 
 
-def Index(template_context, Base=Base):
-    template_context.update({
-        "Base": Base,
-    })
-
+def Index(Base=Base):
     return """
         <Base title="Index">
             <h2>Index</h2>
