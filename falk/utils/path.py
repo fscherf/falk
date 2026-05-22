638b4fc6a240add1c77562a736ce2f927d273995
Florian Scherf <mail@florianscherf.de>
2026-05-22 17:36:21 +0200

    utils: path: add resolve_path
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- falk/utils/path.py
+++ falk/utils/path.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import inspect
 import os
 
@@ -34,3 +35,70 @@ def get_abs_path(caller, path, require_file=False, require_directory=False):
         )
 
     return abs_path
+
+
+def resolve_path(
+        safe_base_paths,
+        unsafe_path,
+        is_directory=None,
+        is_direct_child=None,
+        exists=None,
+):
+
+    """
+    Resolves an unsafe, user provided path, like a static URL, and resolves it
+    against a list of safe base paths.
+    If the unsafe path tries to leave one of the matching base paths,
+    a `FileNotFoundError` is raised.
+
+    All check flags are tri-states: If `exists` is set to `None`, no checks
+    are executed. If set to `True`, the file needs to exist.
+    """
+
+    # clean unsafe path
+    _unsafe_path = Path(unsafe_path.lstrip("/"))
+
+    # run mandatory checks
+    if is_direct_child is not None:
+        parts = _unsafe_path.parts
+
+        if is_direct_child and len(parts) > 1:
+            raise FileNotFoundError(unsafe_path)
+
+        if not is_direct_child and len(parts) < 2:
+            raise FileNotFoundError(unsafe_path)
+
+    for safe_base_path_string in safe_base_paths:
+        safe_base_path = Path(safe_base_path_string).resolve()
+        unsafe_abs_path = (safe_base_path / _unsafe_path).resolve()
+
+        # run mandatory checks
+        # check if the resolved path is a sub path of the safe base path
+        if not unsafe_abs_path.is_relative_to(safe_base_path):
+            continue
+
+        # run extra checks
+        _is_directory = unsafe_abs_path.is_dir()
+
+        # check if path is a directory
+        if is_directory is not None:
+            if is_directory and not _is_directory:
+                continue
+
+            if not is_directory and _is_directory:
+                continue
+
+        # check if file or directory exsits
+        if exists is not None:
+            _exists = unsafe_abs_path.exists()
+
+            if exists and not _exists:
+                continue
+
+            if not exists and _exists:
+                continue
+
+        # file found
+        return str(unsafe_abs_path)
+
+    raise FileNotFoundError(unsafe_path)
